e2ecf742f48fd660933970df2ee44e5790836aab
Dejan Tomic <dtomic@codevise.de>
2017-07-05 12:09:34 +0200

    Fix not working controls

--- src/ios/BackgroundAudio.m
+++ src/ios/BackgroundAudio.m
@@ -13,11 +13,12 @@
     BOOL ok;
     NSError *setCategoryError = nil;
     ok = [audioSession setCategory:AVAudioSessionCategoryPlayback
-                       withOptions:AVAudioSessionCategoryOptionMixWithOthers
                              error:&setCategoryError];
     if (!ok) {
         NSLog(@"%s setCategoryError=%@", __PRETTY_FUNCTION__, setCategoryError);
     }
+
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
 }
 
 @end
